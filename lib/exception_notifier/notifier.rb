be2c4d757ad2d23f4ac2e89b74f99d1b0237714a
John Wood <john@signalhq.com>
2011-09-29 10:40:32 -0500

    Modified to work with Signal message processing daemons

--- lib/exception_notifier/notifier.rb
+++ lib/exception_notifier/notifier.rb
@@ -20,7 +20,7 @@ class ExceptionNotifier
       end
 
       def default_sections
-        %w(request session environment backtrace)
+        %w(message backtrace)
       end
 
       def default_options
@@ -36,7 +36,7 @@ class ExceptionNotifier
       end
     end
 
-    def exception_notification(env, exception)
+    def exception_notification(env, exception, name, message)
       @env        = env
       @exception  = exception
       @options    = (env['exception_notifier.options'] || {}).reverse_merge(self.class.default_options)
@@ -44,6 +44,8 @@ class ExceptionNotifier
       @request    = ActionDispatch::Request.new(env)
       @backtrace  = clean_backtrace(exception)
       @sections   = @options[:sections]
+      @name       = name
+      @message    = message
       data        = env['exception_notifier.exception_data'] || {}
 
       data.each do |name, value|
